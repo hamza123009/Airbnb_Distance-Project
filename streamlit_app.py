7305096d37e79fa51cf99dd5406a32f6f08a1f3b
hamza123009 <145609847+hamza123009@users.noreply.github.com>
2023-09-21 01:03:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import streamlit as st
 
 # Display title and text
-st.title("Week 1 - Data and visualization")
+st.title("Hamza Amir \nWeek 1 - Data and visualization")
 st.markdown("Here we can see the dataframe created during this project.")
 
 # Read dataframe
